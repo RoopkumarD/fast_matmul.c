4dd7d8c831bc8a80a0565097a9c791b4a76b0db6
RoopkumaD <dasroopkumar5@gmail.com>
2024-12-14 21:14:19 +0530

    general row kernel matmul done

--- rmk_general.c
+++ rmk_general.c
@@ -6,14 +6,34 @@
 // because we are matrix multiplying one vector to other thus one element
 // at once looking from both direction
 
+// Had to create buffer for C as opposed to article because my machine supports
+// till SSE_4.2 thus my machine doesn't support maskload and maskstore intrinsic
+
 #include "matrix.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <xmmintrin.h>
 
 #define MR 4
 #define NR 12
+
+void print_othermat(float *mat, int rows, int cols) {
+    printf("[");
+    for (int i = 0; i < rows; i++) {
+        printf("%s", (i != 0) ? " [" : "[");
+        for (int j = 0; j < cols; j++) {
+            printf("%f", mat[i * cols + j]);
+            if (j != cols - 1) {
+                printf(", ");
+            }
+        }
+        printf("%s", (i != rows - 1) ? "],\n" : "]");
+    }
+    printf("]\n");
+    return;
+}
 
 void kernel_4x12(float *Ablock, float *Bblock, float *C, const int M,
                  const int N, const int K) {
@@ -81,11 +101,50 @@ void kernel_matmul(matrix *A, matrix *B, matrix *out) {
     const int N = B->cols;
     const int K = B->rows;
 
+    float Abuffer[MR * K];
+    float Bbuffer[K * NR];
+    float Cbuffer[MR * NR];
+
     for (int i = 0; i < M; i += 4) {
-        int m = (MR < MR - i);
+        int m = ((M - i) < MR) ? (M - i) : MR;
+
+        memcpy(Abuffer, A->data + i * K, sizeof(float) * K * m);
+        memset(Abuffer + m * K, 0, sizeof(float) * K * (MR - m));
+
+        /*
+        printf("======== Abuffer %d =========\n", i);
+        print_othermat(Abuffer, MR, K);
+        */
+
         for (int j = 0; j < N; j += 12) {
-            kernel_4x12(A->data + i * K, B->data + j, out->data + i * N + j, M,
-                        N, K);
+            int n = ((N - j) < NR) ? (N - j) : NR;
+
+            for (int nk = 0; nk < K; nk++) {
+                memcpy(Bbuffer + nk * NR, B->data + nk * N + j,
+                       sizeof(float) * n);
+                memset(Bbuffer + nk * NR + n, 0, sizeof(float) * (NR - n));
+            }
+
+            int cm = 0;
+            for (; cm < m; cm++) {
+                memcpy(Cbuffer + cm * NR, out->data + i * N + j + cm * N,
+                       sizeof(float) * n);
+                memset(Cbuffer + cm * NR + n, 0, sizeof(float) * (NR - n));
+            }
+            memset(Cbuffer + cm * NR, 0, sizeof(float) * NR * (MR - cm));
+
+            /*
+            printf("======== Bbuffer %d =========\n", i);
+            print_othermat(Bbuffer, K, NR);
+            */
+
+            kernel_4x12(Abuffer, Bbuffer, Cbuffer, MR, NR, K);
+
+            // storing result in out
+            for (int cm = 0; cm < m; cm++) {
+                memcpy(out->data + i * N + j + cm * N, Cbuffer + cm * NR,
+                       sizeof(float) * n);
+            }
         }
     }
 
@@ -117,9 +176,15 @@ void compare_matrix(matrix *a, matrix *b) {
 int main(void) {
     srand(time(NULL));
 
-    const int M = 4 * 20;
-    const int N = 12 * 10;
-    const int K = 1 * 100;
+    const int M = rand() % 2000;
+    const int N = rand() % 2000;
+    const int K = rand() % 2000;
+
+    if (M == 0 || N == 0 || K == 0) {
+        printf("Got zero as dimension\n");
+        return 1;
+    }
+    printf("M = %d, N = %d, K = %d\n", M, N, K);
 
     matrix *a = mat_create(M, K);
     if (a == NULL) {
@@ -147,7 +212,15 @@ int main(void) {
     memset(c->data, 0, sizeof(float) * c->total_data);
     memset(d->data, 0, sizeof(float) * d->total_data);
 
+    /*
+    printf("==================== A ====================\n");
+    mat_print(a);
+    printf("==================== B ====================\n");
+    mat_print(b);
+    */
+
     kernel_matmul(a, b, c);
+    printf("Done with kernel matmul\n");
     naive_matmul(a, b, d);
 
     compare_matrix(c, d);
--- todo.md
+++ todo.md
@@ -1,1 +1,13 @@
 - follow the tuts
+  - first completing the masking part
+  - Then write column major and row major in one file and then put
+    all kernel size in arrays and for each size -> check the average flops
+    (first reason if comparing directly in one file make sense for both order)
+    Take different matrix sizes as per std input
+
+    Then lastly take all graph and find either to go with row or column based
+    and then also accounting the fact about which kernel size gives highest flops
+
+    Remember to clean col_kernel_matmul.c to have important function in different files
+  - Why is the article crazy about data being aligned and how can it improve performance
+    as author uses aligned directive for mask bytes also
